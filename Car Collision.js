eaf0a98de5024ea3a6f98305b15cc4b4525eea12
awesomegamer9595 <74835849+awesomegamer9595@users.noreply.github.com>
2020-12-19 21:02:22 -0500

    Add files via upload

---
+++ Car Collision.js
@@ -0,0 +1,43 @@
+var speed,weight;
+var car,wall;
+
+
+function setup() {
+  createCanvas(1600,400);
+  car=createSprite(50,200,100,30)
+  car.shapeColor="white"
+  speed=random(55,90)
+  weight=random(400,1500)
+  wall= createSprite(1500,200,60,height/2)
+  wall.shapeColor="white"
+  createSprite(400, 200, 30, 50);
+}
+
+function draw() {
+  
+  background("black");
+  var deformation=0.5*weight*speed*speed/22509
+  car.velocityX=speed;  
+  
+  if(wall.x-car.x<(car.width+wall.width)/2){
+    car.velocityX=0;
+
+  
+  if(deformation<180 && deformation>100){
+    car.shapeColor=color(230,230,0)
+    
+
+  }
+  if(deformation>180){
+    car.shapeColor="red"
+   
+
+  }
+  if(deformation<100){
+    car.shapeColor=color(0,255,0)
+   
+
+  }
+}
+  drawSprites();
+}
